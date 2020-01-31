b7e78b4aa74fe10ab33be711f0eb8761f16169f2
Laurent Pichon <tech@lescachotieres.com>
2020-01-31 17:35:51 +0100

    Utilisation de la notion de service pour clarifier le controller

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,70 +2,36 @@
 
 namespace App\Controller;
 
-use App\Repository\ProductRepository;
+use App\Services\Cart\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
     /**
      * @Route("/panier", name="cart_index")
-     * @param SessionInterface $session
-     * @param ProductRepository $productRepository
+     * @param CartService $cartService
      * @return Response
      */
-    public function index(SessionInterface $session, ProductRepository $productRepository)
+    public function index(CartService $cartService)
     {
-        // On récupère le contenu du panier dans la session ou on l'initialise à vide si non existant
-        $panier = $session->get('panier', []);
-
-        // On crée un panier avec des valeurs (vide pour le moment)...
-        $panierWithData = [];
-
-        // ... Et on le rempli !
-        foreach ($panier as $id => $quantity) {
-            $panierWithData[] = [
-                'product' => $productRepository->find($id),
-                'quantity' => $quantity
-            ];
-        }
-
-        $total = 0;
-
-        foreach ($panierWithData as $item) {
-            $totalItem = $item['product']->getPrice() * $item['quantity'];
-            $total += $totalItem;
-        }
-
         return $this->render('cart/index.html.twig', [
-            'items' => $panierWithData,
-            'total' => $total
+            'items' => $cartService->getFullCart(),
+            'total' => $cartService->getTotal()
         ]);
     }
 
     /**
      * @Route("/panier/add/{id}", name="cart_add")
      * @param $id
-     * @param SessionInterface $session
+     * @param CartService $cartService
      * @return RedirectResponse
      */
-    public function add($id, SessionInterface $session)
+    public function add($id, CartService $cartService)
     {
-        // On regarde dans la sessiion si un panier existe, sinon on l'initie avec un tableau vide
-        $panier = $session->get('panier', []);
-
-        // Si un produit avec le même id existe déjà alors on incrémente
-        if (!empty($panier[$id])) {
-            $panier[$id]++;
-        } else {
-            $panier[$id] = 1;
-        }
-
-        // Ajout du produit au panier de la session
-        $session->set('panier', $panier);
+       $cartService->add($id);
 
         return $this->redirectToRoute('cart_index');
     }
@@ -73,19 +39,12 @@ class CartController extends AbstractController
     /**
      * @Route("/panier/remove/{id}", name="cart_remove")
      * @param $id
-     * @param SessionInterface $session
+     * @param CartService $cartService
      * @return RedirectResponse
      */
-    public function remove($id, SessionInterface $session)
+    public function remove($id, CartService $cartService)
     {
-        // On récupère le contenu du panier dans la session ou on l'initialise à vide si non existant
-        $panier = $session->get('panier', []);
-
-        if (!empty($panier[$id])) {
-            unset($panier[$id]);
-        }
-
-        $session->set('panier', $panier);
+        $cartService->remove($id);
 
         return $this->redirectToRoute('cart_index');
     }
--- src/Services/Cart/CartService.php
+++ src/Services/Cart/CartService.php
@@ -4,7 +4,75 @@
 namespace App\Services\Cart;
 
 
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 class CartService
 {
-    
+    protected $session;
+    protected $productRepository;
+
+    public function __construct(SessionInterface $session, ProductRepository $productRepository)
+    {
+        $this->session = $session;
+        $this->productRepository = $productRepository;
+    }
+
+    public function add(int $id)
+    {
+        // On regarde dans la sessiion si un panier existe, sinon on l'initie avec un tableau vide
+        $panier = $this->session->get('panier', []);
+
+        // Si un produit avec le même id existe déjà alors on incrémente
+        if (!empty($panier[$id])) {
+            $panier[$id]++;
+        } else {
+            $panier[$id] = 1;
+        }
+
+        // Ajout du produit au panier de la session
+        $this->session->set('panier', $panier);
+    }
+
+    public function remove(int $id)
+    {
+        // On récupère le contenu du panier dans la session ou on l'initialise à vide si non existant
+        $panier = $this->session->get('panier', []);
+
+        if (!empty($panier[$id])) {
+            unset($panier[$id]);
+        }
+
+        $this->session->set('panier', $panier);
+    }
+
+    public function getFullCart() :array
+    {
+        // On récupère le contenu du panier dans la session ou on l'initialise à vide si non existant
+        $panier = $this->session->get('panier', []);
+
+        // On crée un panier avec des valeurs (vide pour le moment)...
+        $panierWithData = [];
+
+        // ... Et on le rempli !
+        foreach ($panier as $id => $quantity) {
+            $panierWithData[] = [
+                'product' => $this->productRepository->find($id),
+                'quantity' => $quantity
+            ];
+        }
+
+        return $panierWithData;
+    }
+
+    public function getTotal() : float
+    {
+        $total = 0;
+
+        foreach ($this->getFullCart() as $item) {
+            $total += $item['product']->getPrice() * $item['quantity'];
+        }
+
+        return $total;
+    }
 }
